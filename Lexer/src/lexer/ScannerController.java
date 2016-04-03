0027be1afcc135c98e1f853ba8dd02a9c850be3b
xelop <adlopez@ic-itcr.ac.cr>
2016-04-03 13:07:11 -0600

    nueva clase controladora
    el nombre no esta muy significativo, si quiere lo cambia. Lo hize para
    evitar tanto static y manejar el proceso de scanner por separado, para
    mas adelante que el main haga todos los pasos del compilador. Comente
    los metodos del main en caso de que crea que esta bien lo que hize los
    elimina sino ahi vemos que hacer

--- Lexer/src/lexer/ScannerController.java
+++ Lexer/src/lexer/ScannerController.java
@@ -1,42 +1,37 @@
 package lexer;
 
+
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.io.Reader;
 import java.util.ArrayList;
 import java.util.Collections;
 
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 
-public class Main {
+/**
+ *
+ * @author Adrian
+ */
+public class ScannerController {
     
-    private static ArrayList<TokenDesplegable> tokens;
-
-    public static void main(String[] args) {
-        String path = "./src/lexer/lexer.flex";
-        generarLexer(path);
-        System.out.println("Analizador Creado. Se procede a scannear:");
-        try{
-            ScannerController scanner = new ScannerController("Prueba.mypy");
-            scanner.Scan();
-            System.out.println("Análisis léxico terminado.");
-        }
-        catch(IOException e){
-            System.out.println("No se pudo abrir el archivo.");
-        }
-    }
-    public static void generarLexer(String path){
-        File file=new File(path);
-        jflex.Main.generate(file);
-    }
-    /*public static void Scan() throws IOException{
+    private ArrayList<TokenDesplegable> tokens;
+    private String path;
+    
+    public ScannerController(String pPath){
+        path = pPath;
         tokens = new ArrayList<TokenDesplegable>();
-        Reader reader = new BufferedReader(new FileReader("Prueba.mypy"));
-        Lexere lexer = new Lexer
-        Lexer lexer = new Lexer(reader);
+    }
+    
+    public void Scan() throws IOException{
         String resultado = "";
+        Reader reader = new BufferedReader(new FileReader(path));
+        Lexer lexer = new Lexer(reader);
         Token currentToken;
         while(true){
             currentToken = lexer.yylex();
@@ -61,7 +56,7 @@ public class Main {
         printTokens();
     }
     
-    private static void printTokens(){
+    private void printTokens(){
         
         Collections.sort(tokens); 
         
@@ -70,7 +65,7 @@ public class Main {
         
     }
     
-    private static void createToken(String pNombre, String pTipo, int pNumeroLinea){
+    private void createToken(String pNombre, String pTipo, int pNumeroLinea){
         
         TokenDesplegable tokenActual = verificarToken(pNombre);
         
@@ -84,12 +79,12 @@ public class Main {
         
     }
     
-    private static TokenDesplegable verificarToken(String pToken){
+    private TokenDesplegable verificarToken(String pToken){
         
         for(TokenDesplegable nuevoToken: tokens ){
             if(nuevoToken.compareName(pToken))
                 return nuevoToken;
         }
         return null;
-    }*/
+    }
 }
